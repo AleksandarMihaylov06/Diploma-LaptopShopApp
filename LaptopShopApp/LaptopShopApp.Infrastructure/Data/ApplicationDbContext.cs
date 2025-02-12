1652bd3b6ae0d551325a84dd064f75bd9001acf5
Sashkoddz <sashkogogo@gmail.com>
2025-02-12 09:22:03 +0200

    Second Part

---
+++ LaptopShopApp/LaptopShopApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LaptopShopApp.Infrastructure.Data.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaptopShopApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Oreders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
